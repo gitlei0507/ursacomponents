dbaaf63cf8e0fbb1f5b49dd8a8b91ee13cbf27da
gitlei0507 <57185557+gitlei0507@users.noreply.github.com>
2026-03-22 16:28:02 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+import UrsaMenu from './components/UrsaMenu/UrsaMenu.vue'
+import UrsaMenuItem from './components/UrsaMenu/UrsaMenuItem.vue'
 import UrsaSearch from './components/UrsaSearch/UrsaSearch.vue'
 import UrsaTable from './components/UrsaTable/UrsaTable.vue'
 
 // 组合式能力按需导出。
+export { useUrsaMenu } from './components/UrsaMenu/useUrsaMenu'
+export { useUrsaMenuItem } from './components/UrsaMenu/useUrsaMenuItem'
 export { useUrsaSearch } from './components/UrsaSearch/useUrsaSearch'
 // 路由工具按需导出。
 export { buildMenuTree, createUrsaMenuRouterToolkit, flattenMenus, getFirstMenuPath, getUrsaMenuIcon, setupUrsaRouterGuard } from './router/index'
 // 组件按需导出。
-export { UrsaSearch, UrsaTable }
+export { UrsaMenu, UrsaMenuItem, UrsaSearch, UrsaTable }
 
 // 全量安装时需要注册的组件列表。
-const components = [UrsaSearch, UrsaTable]
+const components = [UrsaSearch, UrsaTable, UrsaMenu, UrsaMenuItem]
 
 // Vue 插件安装入口：一次性注册所有组件。
 const install = (app) => {
